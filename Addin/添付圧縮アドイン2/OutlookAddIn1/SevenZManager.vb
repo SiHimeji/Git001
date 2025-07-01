423ea34a7ea1ac2bc92a01a8302c27e8bf6081f4
ChiharuFujishiro <si.himeji@gmail.com>
2025-07-01 12:06:50 +0900

    OUTLOCK

---
+++ Addin/添付圧縮アドイン2/OutlookAddIn1/SevenZManager.vb
@@ -0,0 +1,36 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Runtime.InteropServices
+Imports System.Text
+
+Public Class SevenZManager
+    '<Runtime.InteropServices.DllImport("7-zip32.dll")>
+    <Runtime.InteropServices.DllImport("7-zip64.dll")>
+    Private Shared Sub SevenZip(hwnd As IntPtr, szCmdLine As String, szOutput As StringBuilder, dwSize As Integer)
+        'ここには何も記述しません。
+    End Sub
+
+    'Private lockObject As New Object()
+    Public Function fnCompressFilesWithPassword(aryFilePath As List(Of String), s7zFilePath As String, sPassword As String) As Boolean
+
+        Dim sbCmdLine As StringBuilder = New StringBuilder(1024)    ' コマンドライン文字列
+        Dim sbOutput As StringBuilder = New StringBuilder(1024)     ' 7-zip32.dll出力文字
+
+        sbCmdLine.AppendFormat(
+                "a -tzip -hide -mmt=on -y -p" & sPassword & " " & s7zFilePath & " ")
+
+        For Each sFilePath As String In aryFilePath
+            sbCmdLine.AppendFormat("""" & sFilePath & """ ")
+        Next
+
+        Dim sCmdLine As String = sbCmdLine.ToString()
+        '---------------------------------------------------------------------------------
+        '圧縮実行
+        '---------------------------------------------------------------------------------
+        SevenZip(0, sCmdLine, sbOutput, sbOutput.Capacity)
+
+        Return 0
+
+    End Function
+
+End Class
